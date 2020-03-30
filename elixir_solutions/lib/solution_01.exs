078c3dbd94b940ef1d9b0ced8ecb84028c063219
Ben Hodge <ben.hodge@plusonerobotics.com>
2020-03-30 10:20:57 -0500

    first problem

---
+++ elixir_solutions/lib/solution_01.exs
@@ -0,0 +1,21 @@
+defmodule Solution01 do
+  @moduledoc """
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 or 5 below 1000.
+
+  """
+
+  def solve(limit \\ 10) do
+    # make a range
+    0..(limit - 1)
+    |> Enum.filter(fn x -> multiple_of_three(x) || multiple_of_five(x) end)
+    |> Enum.sum()
+  end
+
+  defp multiple_of_three(x), do: rem(x, 3) == 0
+  defp multiple_of_five(x), do: rem(x, 5) == 0
+end
+
+Solution01.solve(1000)
+|> IO.inspect()
