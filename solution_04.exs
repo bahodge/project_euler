e8d5ab9dbd13c7fac3d4d4af78a02d22363b53dc
Ben Hodge <ben.hodge@plusonerobotics.com>
2020-04-01 15:00:54 -0500

    finished

---
+++ solution_04.exs
@@ -0,0 +1,54 @@
+defmodule Solution04 do
+  @moduledoc """
+    A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+    Find the largest palindrome made from the product of two 3-digit numbers.
+  """
+
+  def solve(ceiling, floor), do: solve(ceiling, floor, [])
+  def solve(floor, floor, palindromes), do: Enum.max(palindromes) |> List.first() # Final answer
+  def solve(ceiling, floor, palindromes) do
+    results = get_palindromes(ceiling, floor)
+
+    solve(ceiling - 1, floor, [ results | palindromes])
+  end
+
+  def get_palindromes(ceiling, floor) do
+    multiply_range(ceiling, floor) |> filter_palindromes()
+  end
+
+  defp empty_list?([]), do: true
+  defp empty_list?(_), do: false
+
+  defp multiply_range(ceiling, floor) do
+    ceiling..floor |> Enum.map(fn x -> ceiling * x end)
+  end
+
+  defp filter_palindromes(list), do: filter_palindromes(list, [])
+  defp filter_palindromes([], acc), do: acc
+
+  defp filter_palindromes([head | tail], acc) do
+    case is_palindrome?(head) do
+      true -> filter_palindromes(tail, [head | acc])
+      _ -> filter_palindromes(tail, acc)
+    end
+  end
+
+  def is_palindrome?(number) do
+    split_int =
+      Integer.to_string(number) |> String.split("") |> Enum.filter(fn char -> char != "" end)
+
+    reversed_int = split_int |> reverse_list()
+    reversed_int == split_int
+  end
+
+  defp reverse_list(list), do: reverse_list(list, [])
+  defp reverse_list([], reversed_list), do: reversed_list
+  defp reverse_list([head | tail], reversed_list), do: reverse_list(tail, [head | reversed_list])
+end
+
+ceiling = 999
+floor = 100
+{time, result} = :timer.tc(fn -> Solution04.solve(ceiling, floor) end)
+IO.inspect(result) # 906609
+IO.inspect("#{time / 1000} ms") # 545.721 ms
