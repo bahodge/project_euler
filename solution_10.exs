af83af3e6e64fa0bea6fdb9276c802588b579380
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2020-04-06 23:38:43 -0500

    finish solution 10 and removed the js solution

---
+++ solution_10.exs
@@ -0,0 +1,40 @@
+defmodule Solution08 do
+  @moduledoc """
+  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+  Find the sum of all the primes below two million.
+  """
+
+  def solve(upper_limit) do
+    upper_limit
+    |> get_primes()
+    |> sum_list()
+  end
+
+  defp sum_list(list), do: list |> Enum.reduce(0, &(&1 + &2))
+
+  defp get_primes(upper_limit) do
+    2..upper_limit
+    |> Enum.filter(&is_prime?/1)
+  end
+
+  defp is_prime?(n) when n in [2, 3], do: true
+
+  defp is_prime?(n) do
+    floored = n |> square_root() |> floor_value()
+    !Enum.any?(2..floored, &(rem(n, &1) == 0))
+  end
+
+  defp square_root(n), do: :math.sqrt(n)
+  defp floor_value(n), do: Float.floor(n) |> round()
+end
+
+upper_limit = 2_000_000
+
+{time, result} = :timer.tc(fn -> Solution08.solve(upper_limit) end)
+IO.inspect(result)
+
+# -> 142_913_828_922
+
+IO.inspect("#{time / 1000} ms")
+# -> 11629.909 ms
--- soultion_06.js
+++
@@ -1,9 +0,0 @@
-const square = x => x * x;
-
-const sum = list => list.reduce((acc, x) => x + acc, 0);
-
-const squareList = list => list.reduce((acc, x) => acc + square(x), 0);
-
-const list = [...Array(101).keys()];
-
-console.log(square(sum(list)) - squareList(list));
